e94a918a0a1b6062ec304909263712ab1a2f83cc
ijunfu <ijunfu@163.com>
2024-07-02 11:49:51 +0800

    项目初始化

---
+++ src/FuSvgIcon.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="icon icon-svg"></div>
+</template>
+<script setup lang="ts">
+
+</script>
+<style scoped>
+.icon {
+    width: var(--icon-svg-size);
+    height: var(--icon-svg-size);
+    fill: var(--icon-color, 'currentColor');
+}
+</style>
+
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+
+import FuSvgIcon from './FuSvgIcon.vue'
+
+export default {
+    install(app) {
+        app.component('FuSvgIcon', FuSvgIcon)
+    }
+}
